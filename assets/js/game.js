7c998b0bcbab6ca43a183fac5bef6f21e9bb3bc4
narcpat <narcissearts@gmail.com>
2022-03-02 18:40:11 -0500

    added new health to new enemy robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,8 +8,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators");
+  // repeat and execute as long as the enemy-robot is alive
+  while(enemyHealth > 0) {
 
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -34,7 +34,6 @@ var fight = function(enemyName) {
     enemyName + " attacked " + playerName + ". " +playerName + " now has " + playerHealth + " health remaining."
   );
   
-
   // check player's health
   if (playerHealth <= 0) {
     window.alert(playerName + " has died!");
@@ -60,9 +59,13 @@ var fight = function(enemyName) {
   } else {
   window.alert("You need to choose a valid option. Try again!");
   }
+  } 
 };
 
 // run fight function to start game
   for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    debugger;
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName)
   }
