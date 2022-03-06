ec57ffdcb8f5340da691d0383bad47801e55e029
narcpat <narcissearts@gmail.com>
2022-03-05 21:09:52 -0500

    added local storage function

--- assets/js/game.js
+++ assets/js/game.js
@@ -207,12 +207,6 @@ var startGame = function () {
         if (storeConfirm) {
           shop();
         }
-<<<<<<< HEAD
-=======
-      } else {
-        window.alert("You have lost your robot in battle! GAME OVER!");
-        break;
->>>>>>> develop
       }
     }
   }
@@ -223,15 +217,28 @@ var startGame = function () {
 
 // function to end the entire game
 var endGame = function () {
-  // If player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert(
-      "Great job, you've survived the game! You now have a score of " +
-        playerInfo.money +
-        "."
+  window.alert("The game has now ended. Let's see how you did!");
+
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
+  }
+  // if player has more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(
+      playerInfo.name + " now has the high score of " + playerInfo.money + "!"
     );
   } else {
-    window.alert("You've lost your robot in battle.");
+    alert(
+      playerInfo.name +
+        " did not beat the high score of " +
+        highScore +
+        ". Maybe next time!"
+    );
   }
 
   // Ask player if he wants to play again
